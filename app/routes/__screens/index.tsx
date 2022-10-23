03d7133fa44ca4049801aa9c422456e8a866ca6f
Ben Mackie <benjamin@mackiestudio.com>
2022-10-24 00:24:29 +1100

    small tweak

--- app/routes/__screens/index.tsx
+++ app/routes/__screens/index.tsx
@@ -1,7 +1,9 @@
 // import { SignIn } from '@clerk/remix'
 import type { MetaFunction } from "@remix-run/node"
+import { SignedIn, SignedOut } from "@clerk/remix"
 
 import { metaInfo, genMeta } from '~/meta';
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = genMeta('Home');
 export const handle = { screenName: "Dashboard" };
@@ -9,8 +11,14 @@ export const handle = { screenName: "Dashboard" };
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to {metaInfo.AppTitle}</h1>
-      <p>{metaInfo.AppDescription}</p>
+      <p>Welcome to {metaInfo.AppTitle}</p>
+      <p>&nbsp;</p>
+      <SignedIn>
+        <p>Please select from the above menu</p>
+      </SignedIn>
+      <SignedOut>
+        <p>Please <Link to="/sign-in">sign in</Link> to use this application.</p>
+      </SignedOut>
     </div>
   );
 }
