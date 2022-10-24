4d6c2832b73114c979a6a43365b04c850ee5b240
Ben Mackie <benjamin@mackiestudio.com>
2022-10-25 03:12:28 +1100

    A couple more tweaks

--- app/components/AppLogo.tsx
+++ app/components/AppLogo.tsx
@@ -1,10 +1,14 @@
-import { metaInfo } from '~/meta';
+import { Link } from "react-router-dom";
+import { metaInfo } from "~/meta";
 
 export function AppLogo() {
   return (
-    <img
-      className="h-8 w-8"
-      src={metaInfo.AppLogoUrl}
-      alt={metaInfo.CompanyName} />
+    <Link to="/" className="p-0 m-0">
+      <img
+        className="h-8 w-8"
+        src={metaInfo.AppLogoUrl}
+        alt={metaInfo.CompanyName}
+      />
+    </Link>
   );
 }
